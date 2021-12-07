d6c291ce20f14f33400d26775b679b8ce9849b3f
potocnikluka <luka.potochnik@gmail.com>
2021-12-07 12:17:24 +0100

    np for day 7

--- 2021/days/day07.py
+++ 2021/days/day07.py
@@ -1,15 +1,17 @@
 from advent_day import AdventDay
+import numpy as np
 
 
 class Day07(AdventDay):
     def __init__(self, filename):
-        self.pos = tuple(
-            map(int, self.read_input(filename, lines=False).split(',')))
+        self.pos = np.array(
+            list(map(int, self.read_input(filename, lines=False).split(','))))
 
     def results(self):
         return tuple(self.calculate_fuel_consumption(i == 0) for i in range(2))
 
     def calculate_fuel_consumption(self, constant=True):
-        return min(sum(
-            abs(k - i) if constant else (abs(k - i) * (abs(k - i) + 1) // 2)
-            for k in self.pos) for i in range(min(self.pos), max(self.pos)))
+        return min(np.sum(
+            abs(self.pos - i) if constant else
+            (abs(self.pos - i) * (abs(self.pos - i) + 1) // 2)
+        ) for i in range(min(self.pos), max(self.pos)))
